85d694b7120dab8cac0c45d417ab04a165181b67
silhavyj <jakub.sil@seznam.cz>
2023-02-16 13:41:21 +0100

    Added instruction.hpp/cpp, data_processing.hpp/cpp

---
+++ src/cpu/isa/data_processing.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "instruction.hpp"
+
+namespace zero_mate::cpu::isa
+{
+    class CData_Processing final : public CInstruction
+    {
+    public:
+        enum class NOpcode : std::uint32_t
+        {
+            AND = 0b0000, // operand1 AND operand2
+            EOR = 0b0001, // operand1 EOR operand2
+            SUB = 0b0010, // operand1 - operand2
+            RSB = 0b0011, // operand2 - operand1
+            ADD = 0b0100, // operand1 + operand2
+            ADC = 0b0101, // operand1 + operand2 + carry
+            SBC = 0b0110, // operand1 - operand2 + carry - 1
+            RSC = 0b0111, // operand2 - operand1 + carry - 1
+            TST = 0b1000, // as AND, but result is not written
+            TEQ = 0b1001, // as EOR, but result is not written
+            CMP = 0b1010, // as SUB, but result is not written
+            CMN = 0b1011, // as ADD, but result is not written
+            ORR = 0b1100, // operand1 OR operand2
+            MOV = 0b1101, // operand2(operand1 is ignored)
+            BIC = 0b1110, // operand1 AND NOT operand2(Bit clear)
+            MVN = 0b1111  // NOT operand2(operand1 is ignored)
+        };
+
+        enum class NShift_Type : std::uint32_t
+        {
+            LSL = 0b00, // Logical shift left
+            LSR = 0b01, // Logical shift right
+            ASR = 0b10, // Arithmetic shift right
+            ROR = 0b11  // Rotate right
+        };
+
+        explicit CData_Processing(CInstruction instruction) noexcept;
+
+        [[nodiscard]] bool Is_I_Bit_Set() const noexcept;
+        [[nodiscard]] NOpcode Get_Opcode() const noexcept;
+        [[nodiscard]] bool Is_S_Bit_Set() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Rn() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Rd() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Shift_Amount() const noexcept;
+        [[nodiscard]] NShift_Type Get_Shift_Type() const noexcept;
+        [[nodiscard]] bool Is_Immediate_Shift() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Rs() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Rm() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Rotate() const noexcept;
+        [[nodiscard]] std::uint32_t Get_Immediate() const noexcept;
+    };
+}
---
+++ src/cpu/isa/instruction.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <cstdint>
+
+namespace zero_mate::cpu::isa
+{
+    class CInstruction
+    {
+    public:
+        enum class NCondition : std::uint32_t
+        {
+            EQ = 0b0000, // Z set; equal
+            NE = 0b0001, // Z clear; not equal
+            HS = 0b0010, // C set; unsigned higher or same
+            LO = 0b0011, // C clear; unsigned lower
+            MI = 0b0100, // N set; negative
+            PL = 0b0101, // N clear; positive or zero
+            VS = 0b0110, // V set; overflow
+            VC = 0b0111, // V clear; no overflow
+            HI = 0b1000, // C set and Z clear; unsigned higher
+            LS = 0b1001, // C clear or Z set; unsigned lower or same
+            GE = 0b1010, // N equals V; greater or equal
+            LT = 0b1011, // N not equal to V; less than
+            GT = 0b1100, // Z clear AND (N equals V); greater than
+            LE = 0b1101, // Z set OR (N not equal to V); less than or equal
+            AL = 0b1110, // always
+        };
+
+        enum class NType : std::uint32_t
+        {
+            Data_Processing,
+            Multiply,
+            Multiply_Long,
+            Single_Data_Swap,
+            Branch_And_Exchange,
+            Halfword_Data_Transfer_Register_Offset,
+            Halfword_Data_Transfer_Immediate_Offset,
+            Single_Data_Transfer,
+            Undefined,
+            Block_Data_Transfer,
+            Branch,
+            Coprocessor_Data_Transfer,
+            Coprocessor_Data_Operation,
+            Coprocessor_Register_Transfer,
+            Software_Interrupt,
+            Unknown
+        };
+
+        explicit CInstruction(std::uint32_t value) noexcept;
+
+        [[nodiscard]] std::uint32_t Get_Value() const noexcept;
+        [[nodiscard]] NCondition Get_Condition() const noexcept;
+
+    protected:
+        std::uint32_t m_value;
+    };
+}
