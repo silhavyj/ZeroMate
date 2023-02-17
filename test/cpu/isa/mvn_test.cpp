09116a537777d19d35184573f1f0bd0316cab7e0
silhavyj <jakub.sil@seznam.cz>
2023-02-17 12:37:33 +0100

    Added more mvn_instruction_tests

--- test/cpu/isa/mvn_test.cpp
+++ test/cpu/isa/mvn_test.cpp
@@ -71,3 +71,66 @@ TEST(mvn_instruction, test_04)
     EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::C), true);
     EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::N), true);
 }
+
+TEST(mvn_instruction, test_05)
+{
+    using namespace zero_mate::cpu;
+
+    CARM1176JZF_S cpu{};
+
+    cpu.Execute({
+    { 0x13e0b2bf } // mvnne r11, #0xF000000B
+    });
+
+    EXPECT_EQ(cpu.m_regs[11], 0x0FFFFFF4);
+    EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::C), false);
+    EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::N), false);
+}
+
+TEST(mvn_instruction, test_06)
+{
+    using namespace zero_mate::cpu;
+
+    CARM1176JZF_S cpu{};
+
+    cpu.Execute({
+    { 0xe3a01001 }, // mov r1, #1
+    { 0xe1e00f81 }  // mvn r0, r1, lsl #31
+    });
+
+    EXPECT_EQ(cpu.m_regs[0], 0x7FFFFFFF);
+}
+
+TEST(mvn_instruction, test_07)
+{
+    using namespace zero_mate::cpu;
+
+    CARM1176JZF_S cpu{};
+
+    cpu.Execute({
+    { 0xe3a01001 }, // mov r1, #1
+    { 0xe0300000 }, // eors r0, r0, r0
+    { 0x01f000c1 }  // mvnseq r0, r1, asr #1
+    });
+
+    EXPECT_EQ(cpu.m_regs[0], 0xFFFFFFFF);
+    EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::C), true);
+    EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::N), true);
+}
+
+TEST(mvn_instruction, test_08)
+{
+    using namespace zero_mate::cpu;
+
+    CARM1176JZF_S cpu{};
+
+    cpu.Execute({
+    { 0xe3a01001 }, // mov r1, #1
+    { 0xe3e000fe }, // mvn r0, #0x000000FE
+    { 0xe1f02051 }  // mvns r2, r1, ASR r0
+    });
+
+    EXPECT_EQ(cpu.m_regs[2], 0xFFFFFFFF);
+    EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::C), true);
+    EXPECT_EQ(cpu.m_cspr.Is_Flag_Set(CCSPR::NFlag::N), true);
+}
