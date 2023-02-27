287c2553e5b0150019c1851cedb86ba00ac6f6a2
silhavyj <jakub.sil@seznam.cz>
2023-02-27 16:07:50 +0100

    Added ldmda, ldmdb, and ldmia tests

---
+++ test/arm1176jzf_s/isa/block_data_transfer/ldmda_test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include "arm1176jzf_s/mocks/ram.hpp"
+#include "arm1176jzf_s/core.hpp"
+
+TEST(ldmda_instruction, test_01)
+{
+    using namespace zero_mate::arm1176jzf_s;
+
+    auto ram = std::make_shared<mocks::CRAM>();
+    CCPU_Core cpu{ 0, ram };
+
+    cpu.Execute({
+    { 0xe3a010c8 }, // mov r1, #200
+    { 0xe3a03001 }, // mov r3, #1
+    { 0xe3a04002 }, // mov r4, #2
+    { 0xe3a05003 }, // mov r5, #3
+    { 0xe3a06004 }, // mov r6, #4
+    { 0xe3a0b005 }, // mov r11, #5
+    { 0xe3a0c006 }, // mov r12, #6
+    { 0xe8011878 }, // stmda r1, {r3-r6,r11,r12}
+    { 0xe831007c }  // ldmda r1!, {r2-r6}
+    });
+
+    EXPECT_EQ(cpu.m_regs[2], 2);
+    EXPECT_EQ(cpu.m_regs[3], 3);
+    EXPECT_EQ(cpu.m_regs[4], 4);
+    EXPECT_EQ(cpu.m_regs[5], 5);
+    EXPECT_EQ(cpu.m_regs[6], 6);
+
+    EXPECT_EQ(cpu.m_regs[1], 180);
+}
---
+++ test/arm1176jzf_s/isa/block_data_transfer/ldmia_test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include "arm1176jzf_s/mocks/ram.hpp"
+#include "arm1176jzf_s/core.hpp"
+
+TEST(ldmia_instruction, test_01)
+{
+    using namespace zero_mate::arm1176jzf_s;
+
+    auto ram = std::make_shared<mocks::CRAM>();
+    CCPU_Core cpu{ 0, ram };
+
+    cpu.Execute({
+    { 0xe3a010c8 }, // mov r1, #200
+    { 0xe3a03001 }, // mov r3, #1
+    { 0xe3a04002 }, // mov r4, #2
+    { 0xe3a05003 }, // mov r5, #3
+    { 0xe3a06004 }, // mov r6, #4
+    { 0xe3a0b005 }, // mov r11, #5
+    { 0xe3a0c006 }, // mov r12, #6
+    { 0xe8811878 }, // stmia r1, {r3-r6,r11,r12}
+    { 0xe8b1007c }  // ldmia r1!, {r2-r6}
+    });
+
+    EXPECT_EQ(cpu.m_regs[2], 1);
+    EXPECT_EQ(cpu.m_regs[3], 2);
+    EXPECT_EQ(cpu.m_regs[4], 3);
+    EXPECT_EQ(cpu.m_regs[5], 4);
+    EXPECT_EQ(cpu.m_regs[6], 5);
+
+    EXPECT_EQ(cpu.m_regs[1], 220);
+}
