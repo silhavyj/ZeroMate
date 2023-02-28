72ac621bddf077a16ccd34666298194c7c528070
silhavyj <jakub.sil@seznam.cz>
2023-02-28 08:22:43 +0100

    Made instruction look up table static and moved it to the translation unit

--- src/arm1176jzf_s/isa/isa_decoder.cpp
+++ src/arm1176jzf_s/isa/isa_decoder.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <bitset>
 
 #include "../../utils/math.hpp"
@@ -6,19 +5,32 @@
 
 namespace zero_mate::arm1176jzf_s::isa
 {
+    // clang-format off
+    std::array<CISA_Decoder::TInstruction_Lookup_Record, 8> CISA_Decoder::s_instruction_lookup_table = {{
+    { .mask = 0b0000'1111'1111'1111'1111'1111'1101'0000U, .expected = 0b0000'0001'0010'1111'1111'1111'0001'0000U, .type = CInstruction::NType::Branch_And_Exchange     },
+    { .mask = 0b0000'1110'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'1010'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Branch                  },
+    { .mask = 0b0000'1100'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'0000'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Data_Processing         },
+    { .mask = 0b0000'1111'1100'0000'0000'0000'1111'0000U, .expected = 0b0000'0000'0000'0000'0000'0000'1001'0000U, .type = CInstruction::NType::Multiply                },
+    { .mask = 0b0000'1111'1000'0000'0000'0000'1111'0000U, .expected = 0b0000'0000'1000'0000'0000'0000'1001'0000U, .type = CInstruction::NType::Multiply_Long           },
+    { .mask = 0b0000'1100'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'0100'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Single_Data_Transfer    },
+    { .mask = 0b0000'1110'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'1000'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Block_Data_Transfer     },
+    { .mask = 0b0000'1110'0000'0000'0000'1111'1001'0000U, .expected = 0b0000'0000'0000'0000'0000'0000'1001'0000U, .type = CInstruction::NType::Halfword_Data_Transfer  }
+    }};
+    // clang-format on
+
     CISA_Decoder::CISA_Decoder() noexcept
     {
-        std::sort(INSTRUCTION_LOOKUP_TABLE.begin(),
-                  INSTRUCTION_LOOKUP_TABLE.end(),
+        std::sort(s_instruction_lookup_table.begin(),
+                  s_instruction_lookup_table.end(),
                   [](const auto& record1, const auto& record2) -> bool {
                       return utils::math::Get_Number_Of_Set_Bits(record1.mask) >
                              utils::math::Get_Number_Of_Set_Bits(record2.mask);
                   });
     }
 
-    CInstruction::NType CISA_Decoder::Get_Instruction_Type(CInstruction instruction) const noexcept
+    CInstruction::NType CISA_Decoder::Get_Instruction_Type(CInstruction instruction) noexcept
     {
-        for (const auto& [mask, expected, type] : INSTRUCTION_LOOKUP_TABLE)
+        for (const auto& [mask, expected, type] : s_instruction_lookup_table)
         {
             if ((instruction.Get_Value() & mask) == expected)
             {
--- src/arm1176jzf_s/isa/isa_decoder.hpp
+++ src/arm1176jzf_s/isa/isa_decoder.hpp
@@ -17,22 +17,12 @@ namespace zero_mate::arm1176jzf_s::isa
             CInstruction::NType type;
         };
 
-        // clang-format off
-        std::array<TInstruction_Lookup_Record, 8> INSTRUCTION_LOOKUP_TABLE = {{
-            { .mask = 0b0000'1111'1111'1111'1111'1111'1101'0000U, .expected = 0b0000'0001'0010'1111'1111'1111'0001'0000U, .type = CInstruction::NType::Branch_And_Exchange     },
-            { .mask = 0b0000'1110'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'1010'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Branch                  },
-            { .mask = 0b0000'1100'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'0000'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Data_Processing         },
-            { .mask = 0b0000'1111'1100'0000'0000'0000'1111'0000U, .expected = 0b0000'0000'0000'0000'0000'0000'1001'0000U, .type = CInstruction::NType::Multiply                },
-            { .mask = 0b0000'1111'1000'0000'0000'0000'1111'0000U, .expected = 0b0000'0000'1000'0000'0000'0000'1001'0000U, .type = CInstruction::NType::Multiply_Long           },
-            { .mask = 0b0000'1100'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'0100'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Single_Data_Transfer    },
-            { .mask = 0b0000'1110'0000'0000'0000'0000'0000'0000U, .expected = 0b0000'1000'0000'0000'0000'0000'0000'0000U, .type = CInstruction::NType::Block_Data_Transfer     },
-            { .mask = 0b0000'1110'0000'0000'0000'1111'1001'0000U, .expected = 0b0000'0000'0000'0000'0000'0000'1001'0000U, .type = CInstruction::NType::Halfword_Data_Transfer  },
-        }};
-        // clang-format on
-
     public:
         CISA_Decoder() noexcept;
 
-        [[nodiscard]] CInstruction::NType Get_Instruction_Type(CInstruction instruction) const noexcept;
+        [[nodiscard]] static CInstruction::NType Get_Instruction_Type(CInstruction instruction) noexcept;
+
+    private:
+        static std::array<TInstruction_Lookup_Record, 8> s_instruction_lookup_table;
     };
 }
