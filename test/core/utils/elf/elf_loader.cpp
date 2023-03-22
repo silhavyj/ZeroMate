4ead4349da6528dff3c273f7cf807b434d883539
Jakub Šilhavý <jakub.sil@seznam.cz>
2023-03-22 15:09:52 +0100

    Added #ifdef _WIN32 to the path definition, hoping to solve the error

--- test/core/utils/elf/elf_loader.cpp
+++ test/core/utils/elf/elf_loader.cpp
@@ -28,12 +28,22 @@ static void Run_Test(const char* filename, std::uint32_t expected_value)
 
 TEST(elf_loader, test_01)
 {
-    const std::filesystem::path path = "../../../../test/core/utils/elf/source_files/test_01/kernel.elf";
+#ifdef _WIN32
+    const std::string path = "..\\..\\..\\..\\test\\core\\utils\\elf\\source_files\\test_01\\kernel.elf";
+#else
+    const std::string path = "../../../../test/core/utils/elf/source_files/test_01/kernel.elf";
+#endif
+
     Run_Test(path.c_str(), 2);
 }
 
 TEST(elf_loader, test_02)
 {
-    const std::filesystem::path path = "../../../../test/core/utils/elf/source_files/test_02/kernel.elf";
+#ifdef _WIN32
+    const std::string path = "..\\..\\..\\..\\test\\core\\utils\\elf\\source_files\\test_02\\kernel.elf";
+#else
+    const std::string path = "../../../../test/core/utils/elf/source_files/test_02/kernel.elf";
+#endif
+
     Run_Test(path.c_str(), 6);
 }
