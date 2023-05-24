a25c5963844e092516ceaf11d8028b4b48c20f3c
silhavyj <jakub.sil@seznam.cz>
2023-05-24 21:16:32 +0200

    Added comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "gui/gui.hpp"
 
+// ---------------------------------------------------------------------------------------------------------------------
+/// \brief Entry point of the application
+/// \param argc Total number of arguments passed in the command line
+/// \param argv Arguments passed in the command line
+/// \return Exit code
+// ---------------------------------------------------------------------------------------------------------------------
 int main(int argc, const char* argv[])
 {
     return zero_mate::gui::Main_GUI(argc, argv);
