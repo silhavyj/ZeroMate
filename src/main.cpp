74d056aee39e1770fd4282b542050f6aac803b46
silhavyj <jakub.sil@seznam.cz>
2023-05-24 21:22:23 +0200

    Modified doxygen.yml

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+// ---------------------------------------------------------------------------------------------------------------------
+/// \file main.cpp
+/// \date 24. 05. 2023
+/// \author Jakub Silhavy (jakub.silhavy.cz@gmail.com)
+///
+/// \brief This file defines and implements the main function of the application.
+// ---------------------------------------------------------------------------------------------------------------------
+
+// Project file imports
+
 #include "gui/gui.hpp"
 
 // ---------------------------------------------------------------------------------------------------------------------
-/// \brief Entry point of the application
+/// \brief Entry point of the application.
 /// \param argc Total number of arguments passed in the command line
 /// \param argv Arguments passed in the command line
 /// \return Exit code
