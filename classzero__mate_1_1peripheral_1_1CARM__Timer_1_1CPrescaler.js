3b3fe85f61afcb9e24bdf103237e4706958e3885
silhavyj <jakub.silhavy.cz@gmail.com>
2023-05-27 18:21:36 +0000

    Deploying to gh-pages from  @ 5d1d722838d0da8c2aa87a4a8c9a0ad1b9a6bcea 🚀

--- classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.js
+++ classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.js
@@ -5,7 +5,7 @@ var classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler =
       [ "Prescale_256", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#a977c6c6df65cf3781caa89afb8b5a19ca92599d7b31a84cf6efbf57e127f7eb9b", null ]
     ] ],
     [ "CPrescaler", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#a440710dcce7be4e7dd1940bda5de1f0e", null ],
-    [ "Prescale_Cycle_Passed", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#a4e0c606ec657b298b2d5c6db851e9ecf", null ],
+    [ "operator()", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#a8d0194c14063f78a0903cfb326dc40b3", null ],
     [ "Set_Limit", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#a781c1f391f64fd775d32b65dee2ba98d", null ],
     [ "Update_Counter", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#a4d59e407ab56764fc2088ad6efc3f378", null ],
     [ "m_counter", "classzero__mate_1_1peripheral_1_1CARM__Timer_1_1CPrescaler.html#ab8d6a61f81cd3dfe0c1738e4acae2fc5", null ],
