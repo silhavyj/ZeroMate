6f95df9bdaff61766fbc400d4a3f276da3dc038d
silhavyj <jakub.sil@seznam.cz>
2023-06-04 09:06:32 +0200

    Added REQUIRED dot into find_package(Doxygen)

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,15 +1,15 @@
-# check if Doxygen is installed
-find_package(Doxygen)
+# Check if Doxygen is installed
+find_package(Doxygen REQUIRED dot)
 
 if (DOXYGEN_FOUND)
-    # note the option ALL which allows to build the docs together with the application
+    # Note the option ALL which allows to build the docs together with the application.
     add_custom_target(doc_doxygen ALL
-        COMMAND ${DOXYGEN_EXECUTABLE}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        COMMENT "Generating documentation with Doxygen"
-        VERBATIM)
+            COMMAND ${DOXYGEN_EXECUTABLE}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMENT "Generating documentation with Doxygen"
+            VERBATIM)
 else (DOXYGEN_FOUND)
-  message("Doxygen need to be installed to generate the doxygen documentation")
+    message("Doxygen need to be installed to generate the doxygen documentation")
 endif (DOXYGEN_FOUND)
 
 # EOF
