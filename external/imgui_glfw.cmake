38bcf28fe0cf92b71077f65de102ced99ef78bea
silhavyj <jakub.sil@seznam.cz>
2023-06-15 09:12:56 +0200

    #58 Build imgui_glfw as a shared lib on Linux

--- external/imgui_glfw.cmake
+++ external/imgui_glfw.cmake
@@ -1,7 +1,13 @@
 find_package(OpenGL REQUIRED)
 
+if (MSVC)
+    set(LIB_TYPE STATIC)
+else()
+    set(LIB_TYPE SHARED)
+endif()
+
 add_library(
-    imgui_glfw STATIC
+    imgui_glfw ${LIB_TYPE}
     ${CMAKE_CURRENT_SOURCE_DIR}/imgui/imgui.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/imgui/imgui_tables.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/imgui/imgui_widgets.cpp
@@ -10,10 +16,6 @@ add_library(
     ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_glfw.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/imgui/backends/imgui_impl_opengl3.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/ImGuiFileDialog/ImGuiFileDialog.cpp)
-
-set_target_properties(imgui_glfw PROPERTIES
-    WINDOWS_EXPORT_ALL_SYMBOLS ON
-    IMPORTED_IMPLIB ${CMAKE_CURRENT_BINARY_DIR}/imgui_glfw.lib)
 
 target_include_directories(
     imgui_glfw
@@ -29,10 +31,15 @@ target_link_libraries(
         glfw
         OpenGL::GL)
 
-set(output_directory ${PROJECT_SOURCE_DIR}/output)
+if (MSVC)
+    set_target_properties(
+        imgui_glfw PROPERTIES
+        WINDOWS_EXPORT_ALL_SYMBOLS ON
+        IMPORTED_IMPLIB ${CMAKE_CURRENT_BINARY_DIR}/imgui_glfw.lib)
 
-file(MAKE_DIRECTORY ${output_directory})
-
-add_custom_command(
-    TARGET imgui_glfw POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:imgui_glfw> ${output_directory})
+    set(output_directory ${PROJECT_SOURCE_DIR}/output)
+    add_custom_command(
+        TARGET imgui_glfw POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${output_directory}
+        COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:imgui_glfw> ${output_directory})
+endif()
