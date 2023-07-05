7cbd43192714adaacd5245d1ff07b89eec90a6ad
silhavyj <jakub.silhavy.cz@gmail.com>
2023-07-05 12:45:53 +0200

    #81 A bit of refactoring in registers_window.cpp/hpp

--- src/gui/gui.hpp
+++ src/gui/gui.hpp
@@ -1,17 +1,42 @@
+// ---------------------------------------------------------------------------------------------------------------------
+/// \file gui.hpp
+/// \date 05. 07. 2023
+/// \author Jakub Silhavy (jakub.silhavy.cz@gmail.com)
+///
+/// \brief This file defines the main GUI entry function that takes care of creating and rendering all windows.
+// ---------------------------------------------------------------------------------------------------------------------
+
 #pragma once
 
+// STL imports (excluded from Doxygen)
+/// \cond
 #include <cstdint>
+/// \endcond
 
 namespace zero_mate::gui
 {
     namespace config
     {
+        /// Path to the font used by the application
         inline const char* const Font_Path = "fonts/Cousine-Regular.ttf";
+
+        /// Path to the icons used in the GUI of the application
         inline const char* const Icons_Path = "icons/fa-solid-900.ttf";
+
+        /// Path to the logo of the application
         inline const char* const Window_Icon_Path = "logos/logo.png";
 
     } // namespace config
 
+    // -----------------------------------------------------------------------------------------------------------------
+    /// \brief Main entry point of the GUI of the application.
+    ///
+    /// Its main purpose is to create and render the GUI of the application.
+    ///
+    /// \param argc Number of arguments passed into the function
+    /// \param argv Arguments themselves
+    /// \return Return code
+    // -----------------------------------------------------------------------------------------------------------------
     int Main_GUI(int argc, const char* argv[]);
 
 } // namespace zero_mate::gui
--- src/gui/window.hpp
+++ src/gui/window.hpp
@@ -1,20 +1,78 @@
+// ---------------------------------------------------------------------------------------------------------------------
+/// \file window.hpp
+/// \date 05. 07. 2023
+/// \author Jakub Silhavy (jakub.silhavy.cz@gmail.com)
+///
+/// \brief This file defines an interface for all windows that will be rendered by the application.
+// ---------------------------------------------------------------------------------------------------------------------
+
 #pragma once
 
+// STL imports (excluded from Doxygen)
+/// \cond
 #include <memory>
+/// \endcond
+
+// 3rd party library includes
+
+#include <imgui/imgui.h>
 
 namespace zero_mate
 {
+    // -----------------------------------------------------------------------------------------------------------------
+    /// \class IGUI_Window
+    /// \brief Common interface for all GUI windows.
+    // -----------------------------------------------------------------------------------------------------------------
     class IGUI_Window
     {
     public:
+        // clang-format off
+        /// Flags defining the looks of all tables.
+        static constexpr ImGuiTableFlags Table_Flags = ImGuiTableFlags_SizingFixedFit |
+                                                       ImGuiTableFlags_RowBg |
+                                                       ImGuiTableFlags_Borders |
+                                                       ImGuiTableFlags_Resizable |
+                                                       ImGuiTableFlags_Reorderable |
+                                                       ImGuiTableFlags_Hideable;
+        // clang-format on
+
+    public:
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Creates an instance of the class (default non-parameterized constructor).
+        // -------------------------------------------------------------------------------------------------------------
         IGUI_Window() = default;
+
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Destroys (deletes) the object from the memory.
+        // -------------------------------------------------------------------------------------------------------------
         virtual ~IGUI_Window() = default;
 
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Deleted copy constructor (rule of five).
+        // -------------------------------------------------------------------------------------------------------------
         IGUI_Window(const IGUI_Window&) = delete;
+
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Deleted assignment operator (rule of five).
+        /// \return Instance of this class
+        // -------------------------------------------------------------------------------------------------------------
         IGUI_Window& operator=(const IGUI_Window&) = delete;
+
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Deleted move constructor (rule of five).
+        // -------------------------------------------------------------------------------------------------------------
         IGUI_Window(IGUI_Window&&) = delete;
+
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Deleted assignment operator with an r-value reference (rule of five).
+        /// \return Instance of this class
+        // -------------------------------------------------------------------------------------------------------------
         IGUI_Window& operator=(IGUI_Window&&) = delete;
 
+        // -------------------------------------------------------------------------------------------------------------
+        /// \brief Renders the window.
+        // -------------------------------------------------------------------------------------------------------------
         virtual void Render() = 0;
     };
-}
+
+} // namespace zero_mate
