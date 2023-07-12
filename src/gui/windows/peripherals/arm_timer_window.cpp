43b2f6c88dad60dec336bbf4f362a49d09f2b719
Kuba Šilhavý <jakub.silhavy.cz@gmail.com>
2023-07-12 16:27:53 +0200

    #81 Refactored and added comments to arm_timer_window.hpp/cpp

--- src/gui/windows/peripherals/arm_timer_window.cpp
+++ src/gui/windows/peripherals/arm_timer_window.cpp
@@ -1,4 +1,16 @@
-#include <fmt/format.h>
+// ---------------------------------------------------------------------------------------------------------------------
+/// \file arm_timer_window.cpp
+/// \date 12. 07. 2023
+/// \author Jakub Silhavy (jakub.silhavy.cz@gmail.com)
+///
+/// \brief This file implements a window that visualizes the contents of the ARM timer registers.
+// ---------------------------------------------------------------------------------------------------------------------
+
+// 3rd party libraries
+
+#include "fmt/format.h"
+
+// Project file imports
 
 #include "arm_timer_window.hpp"
 
@@ -11,130 +23,121 @@ namespace zero_mate::gui
 
     void CARM_Timer_Window::Render()
     {
+        // Render the window.
         if (ImGui::Begin("ARM timer"))
         {
+            // Render ARM timer registers.
             Render_Registers();
+
+            // Render the bits of the control register.
             Render_Control_Register();
         }
 
         ImGui::End();
     }
 
-    void CARM_Timer_Window::Render_Registers()
+    void CARM_Timer_Window::Render_Table(const char* title,
+                                         const std::vector<std::pair<std::string, std::string>>& data)
     {
-        if (ImGui::BeginTable("##ARM_timer_registers", 2, Table_Flags))
+        if (ImGui::BeginTable(title, 2, Table_Flags))
         {
-            ImGui::TableSetupColumn("Register", ImGuiTableColumnFlags_WidthFixed);
-            ImGui::TableSetupColumn("Value", ImGuiTableColumnFlags_WidthStretch);
-
-            ImGui::TableHeadersRow();
-
-            ImGui::TableNextRow();
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Load");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s",
-                        fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Load)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Value");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Value)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("IRQ Clear");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::IRQ_Clear)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("IRQ Raw");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::IRQ_Raw)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("IRQ Masked");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::IRQ_Masked)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Reload");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Reload)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Pre divider");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Pre_Divider)).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Free running");
-            ImGui::TableNextColumn();
-            ImGui::Text(
-            "%s",
-            fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Free_Running)).c_str());
-
-            ImGui::EndTable();
-        }
-    }
-
-    void CARM_Timer_Window::Render_Control_Register()
-    {
-        const auto control_reg = m_arm_timer->Get_Control_Reg();
-        ImGui::Text("Control register");
-
-        if (ImGui::BeginTable("##ARM_timer_control_register", 2, Table_Flags))
-        {
+            // Table headings
             ImGui::TableSetupColumn("Field", ImGuiTableColumnFlags_WidthFixed);
             ImGui::TableSetupColumn("Value", ImGuiTableColumnFlags_WidthStretch);
 
             ImGui::TableHeadersRow();
 
-            ImGui::TableNextRow();
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Counter 32b");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", fmt::format("{:b}", control_reg.Counter_32b).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Prescaler");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", fmt::format("{:b}", control_reg.Prescaler).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Interrupt enabled");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", fmt::format("{:b}", control_reg.Interrupt_Enabled).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Timer enabled");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", fmt::format("{:b}", control_reg.Timer_Enabled).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Halt in debug break");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", fmt::format("{:b}", control_reg.Halt_In_Debug_Break).c_str());
-
-            ImGui::TableNextColumn();
-            ImGui::Text("Free running");
-            ImGui::TableNextColumn();
-            ImGui::Text("%s", fmt::format("{:b}", control_reg.Free_Running).c_str());
+            // Render table data row by row.
+            for (const auto& [key, value] : data)
+            {
+                ImGui::TableNextRow();
+                ImGui::TableNextColumn();
+                ImGui::Text("%s", key.c_str());
+                ImGui::TableNextColumn();
+                ImGui::Text("%s", value.c_str());
+            }
 
             ImGui::EndTable();
         }
     }
-}
+
+    void CARM_Timer_Window::Render_Registers()
+    {
+        ImGui::Text("Registers");
+
+        // clang-format off
+        Render_Table("ARM_timer_registers", {
+            {
+                "Load",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Load))
+            },
+            {
+                "Value",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Value))
+            },
+            {
+                "IRQ Clear",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::IRQ_Clear))
+            },
+            {
+                "IRQ Raw",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::IRQ_Raw))
+            },
+            {
+                "IRQ Masked",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::IRQ_Masked))
+            },
+            {
+                "Reload",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Reload))
+            },
+            {
+                "Pre divider",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Pre_Divider))
+            },
+            {
+                "Free running",
+                fmt::format("0x{:08X}", m_arm_timer->Get_Reg(peripheral::CARM_Timer::NRegister::Free_Running))
+            }
+        });
+        // clang-format on
+    }
+
+    void CARM_Timer_Window::Render_Control_Register()
+    {
+        ImGui::Text("Control register");
+
+        // Get the control register.
+        const auto control_reg = m_arm_timer->Get_Control_Reg();
+
+        // clang-format off
+        Render_Table("ARM_timer_control_register", {
+            {
+                "Counter 32b",
+                fmt::format("{:b}", control_reg.Counter_32b)
+            },
+            {
+                "Prescaler",
+                fmt::format("{:b}", control_reg.Prescaler)
+            },
+            {
+                "Interrupt enabled",
+                fmt::format("{:b}", control_reg.Interrupt_Enabled)
+            },
+            {
+                "Timer enabled",
+                fmt::format("{:b}", control_reg.Timer_Enabled)
+            },
+            {
+                "Halt in debug break",
+                fmt::format("{:b}", control_reg.Halt_In_Debug_Break)
+            },
+            {
+                "Free running",
+                fmt::format("{:b}", control_reg.Free_Running)
+            }
+        });
+        // clang-format on
+    }
+
+} // namespace zero_mate::gui
