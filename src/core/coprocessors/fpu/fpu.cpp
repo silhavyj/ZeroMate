786964d5f4c5ed62b1b1d3b33241f410b33d6aeb
Kuba Šilhavý <jakub.silhavy.cz@gmail.com>
2023-07-15 15:25:51 +0200

    #92 Added basic files for FPU implementation

--- src/core/coprocessors/fpu/fpu.cpp
+++ src/core/coprocessors/fpu/fpu.cpp
@@ -1,3 +1,25 @@
-//
-// Created by Kuba on 15/07/2023.
-//
+#include "fpu.hpp"
+
+namespace zero_mate::coprocessor::cp10
+{
+    CFPU::CFPU(arm1176jzf_s::CCPU_Context& cpu_context)
+    : ICoprocessor{ cpu_context }
+    {
+    }
+
+    void CFPU::Reset()
+    {
+    }
+
+    void CFPU::Perform_Data_Operation([[maybe_unused]] arm1176jzf_s::isa::CCoprocessor_Data_Operation instruction)
+    {
+    }
+
+    void CFPU::Perform_Data_Transfer([[maybe_unused]] arm1176jzf_s::isa::CCoprocessor_Data_Transfer instruction)
+    {
+    }
+
+    void CFPU::Perform_Register_Transfer([[maybe_unused]] arm1176jzf_s::isa::CCoprocessor_Reg_Transfer instruction)
+    {
+    }
+}
--- src/core/coprocessors/fpu/fpu.hpp
+++ src/core/coprocessors/fpu/fpu.hpp
@@ -1,8 +1,21 @@
-//
-// Created by Kuba on 15/07/2023.
-//
+#pragma once
 
-#ifndef ZEROMATE_FPU_HPP
-#define ZEROMATE_FPU_HPP
+#include "../coprocessor.hpp"
 
-#endif //ZEROMATE_FPU_HPP
+namespace zero_mate::coprocessor::cp10
+{
+    class CFPU final : public ICoprocessor
+    {
+    public:
+        static constexpr std::uint32_t ID = 10;
+
+    public:
+        explicit CFPU(arm1176jzf_s::CCPU_Context& cpu_context);
+
+        void Reset() override;
+
+        void Perform_Register_Transfer(arm1176jzf_s::isa::CCoprocessor_Reg_Transfer instruction) override;
+        void Perform_Data_Transfer(arm1176jzf_s::isa::CCoprocessor_Data_Transfer instruction) override;
+        void Perform_Data_Operation(arm1176jzf_s::isa::CCoprocessor_Data_Operation instruction) override;
+    };
+}
