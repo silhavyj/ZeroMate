caf48eef072641be4d0eec9809cc42410227015e
Kuba Šilhavý <jakub.silhavy.cz@gmail.com>
2023-07-18 18:56:04 +0200

    #92 Removed the renamed folder

--- examples/18-edf/kernel/include/board/rpi0/hal/intdef.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-using int8_t = char;
-using int16_t = short;
-using int32_t = int;
-
-using uint8_t = unsigned char;
-using uint16_t = unsigned short;
-using uint32_t = unsigned int;
