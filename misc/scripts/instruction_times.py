deb8381dc95ee5a725cd74e5e055bcbbf808641f
silhavyj <jakub.silhavy.cz@gmail.com>
2024-04-20 20:37:11 +0200

    Hopefully final modifications of the DP

--- misc/scripts/instruction_times.py
+++ misc/scripts/instruction_times.py
@@ -5,11 +5,13 @@ with open('instruction_times.json', 'r') as f:
     
 total_count = 0
 weighted_time = 0
+total_execution_time = 0
     
 for instruction in stats['instructions']:
     total_count += instruction['count']
     weighted_time += instruction['time'] * instruction['count']
-    instruction['weight'] = round((instruction['time'] / 1e9) * instruction['count'], 3)
+    instruction['weight'] = (instruction['time'] / 1e9) * instruction['count']
+    total_execution_time += instruction['weight']
     instruction['time'] = round((1e9 / float(instruction['time'])) / 1e6, 2)
     
 for instruction in stats['instructions']:
@@ -19,11 +21,18 @@ stats['instructions'] = sorted(stats['instructions'], reverse=True, key=lambda i
 
 assert_count = 0.0
 
+print(f'Total execution time {total_execution_time}')
+
 for instruction in stats['instructions']:
-    print(f"{instruction['count']}\t{instruction['time']}\t{instruction['weight']}\t{instruction['type']}")
+    print(f"{instruction['count']}\t{instruction['time']}\t{round((instruction['weight'] / total_execution_time) * 100.0, 2)}\t{instruction['type']}")
     assert_count += instruction['count']
     
 weighted_time /= total_count
+
+print()
+
+print(f'{float(weighted_time)} ns * x = 1s (1e9)')
+print(f'x = 1e9 / {float(weighted_time)} = {1e9 / float(weighted_time)} inst/s = {(1e9 / float(weighted_time)) / 1e6} M_inst/s')
 weighted_time = round((1e9 / float(weighted_time)) / 1e6, 2)
 
 print()
